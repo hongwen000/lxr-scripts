bfe9a78343a93e62ba6419cb4423c2647cca5f28
lixinrui <lixinrui000@gmail.com>
2023-02-19 00:16:29 +0800

    c

--- train_xinhai.sh
+++ train_xinhai.sh
@@ -1,12 +1,12 @@
 # LoRA train script by @Akegarasu
 
 # Train data path | 设置训练用模型、图片
-pretrained_model="./sd-models/model.ckpt" # base model path | 底模路径
+pretrained_model="./sd-models/Chilloutmix-Ni.safetensors" # base model path | 底模路径
 train_data_dir="./train/xinhai" # train dataset path | 训练数据集路径
 
 # Train related params | 训练相关参数
 resolution="512,640" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
-batch_size=4 # batch size
+batch_size=16 # batch size
 max_train_epoches=20 # max train epoches | 最大训练 epoch
 save_every_n_epochs=2 # save every n epochs | 每 N 个 epoch 保存一次
 network_dim=64 # network dim | 常用 4~128，不是越大越好
@@ -23,7 +23,7 @@ lr_scheduler="cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts"
 lr_warmup_steps=0 # warmup steps | 仅在 lr_scheduler 为 constant_with_warmup 时需要填写这个值
 
 # Output settings | 输出设置
-output_name="xinhai" # output model name | 模型保存名称
+output_name="xinhai-chillout" # output model name | 模型保存名称
 save_model_as="safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
 
 # 其他设置
