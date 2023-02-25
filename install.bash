d549fe835910bc3219c511f6bd00b5672bc15642
V3GWR36YG <hongwen000@outlook.com>
2023-02-26 03:38:01 +0800

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ echo "Installing deps..."
 cd ./sd-scripts
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install --upgrade -r requirements.txt
-pip install -U -I --no-deps https://github.com/ninele7/xfromers_builds/releases/download/3393900949/xformers-0.0.14.dev0-cp310-cp310-linux_x86_64.whl
+pip install -U -I --no-deps https://ghproxy.com/https://github.com/ninele7/xfromers_builds/releases/download/3393900949/xformers-0.0.14.dev0-cp310-cp310-linux_x86_64.whl
 pip install triton==2.0.0.dev20221202
 
 echo "Install completed"
